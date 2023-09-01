ac56819232523de8b68e6e6b07412f5459a42bc0
sb-bipin <bipinpoudel406@gmail.com>
2023-09-01 09:04:01 +0545

    changes filter of service

---
+++ home/urls.py
@@ -0,0 +1,51 @@
+"""Hello URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.conf.urls.static import static
+from django.conf import settings
+from django.contrib import admin
+from django.urls import path
+from home import views
+
+
+admin.site.site_header = "Hamro Kotha Admin Panel"
+admin.site.site_title = "HamroKotha Admin Portal"
+admin.site.index_title = "Welcome to Hamro_Kotha"
+
+
+urlpatterns = [
+    path("", views.logout, name='logouthome'),
+    path("home", views.home, name='home'),
+    path("services", views.services, name='services'),
+    path("contact", views.contact, name='contact'),
+    path("login", views.Login, name='Login'),
+    path("signup", views.signup, name='signup'),
+    path('gallery', views.gallery, name="gallery"),
+    path('logout', views.logout, name="Logout"),
+    path('service_details/<int:service_id>/',
+         views.service_details, name='service_details'),
+    path('about', views.about, name="about"),
+    path('loginabout', views.loginabout, name="loginabout"),
+    path('logoutService', views.logoutService, name="logoutService"),
+    # path("deleteservice", views.deleteservice, name='deleteservice'),
+
+
+
+]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
---
+++ home/views.py
@@ -0,0 +1,163 @@
+from django.db import models
+from django.http import JsonResponse
+import logging
+from .forms import PropertyDetailsForm, RoomsDetailsForm, HousesDetailsForm
+from tkinter import Image
+from django.shortcuts import get_object_or_404, render, redirect
+import pytest
+
+# from .models import RoomsDetails
+from .forms import signupform
+from django.contrib.auth import login, authenticate
+from django.contrib.auth import authenticate, login
+from datetime import datetime
+from django.shortcuts import redirect, render, HttpResponse
+from home.models import Contact, Property, Rooms
+from django.contrib import messages
+from .forms import signupform
+from django.views.decorators.csrf import csrf_protect
+
+
+def logout(request):
+    return render(request, "logouthome.html")
+
+# Create your views here.
+
+
+def about(request):
+    return render(request, "about.html")
+
+
+def loginabout(request):
+    return render(request, "loginabout.html")
+
+
+# def index(request):
+#     return render(request, "index.html")
+
+
+def home(request):
+    userproperties = Property.objects.filter(owner_id=request.user.id)
+    return render(request, "index.html", {'userproperties': userproperties})
+
+
+def services(request):
+    filter_type = request.GET.get("filter")
+
+    if filter_type == "ac":
+        allimages = Property.objects.filter(rooms__acfan="ac")
+    elif filter_type == "lowhighprice":
+        allimages = Property.objects.all().order_by("price")
+    elif filter_type == "attachbathroom":
+        allimages = Property.objects.filter(rooms__attachedbathroom="YES")
+    else:
+        allimages = Property.objects.all().order_by("-id")
+
+    context = {'propertydetails': allimages}
+    return render(request, 'services.html', context)
+
+
+def service_details(request, service_id):
+    # services = get_object_or_404(Property, id=service_id)
+    services = Property.objects.filter(id=service_id)
+    return render(request, 'service_details.html', {'services': services})
+
+
+def logoutService(request):
+    logoutServices = Property.objects.all()[:12]
+    # messages.error(request, "Please log in to see more.")
+    return render(request, 'logoutService.html', {'logoutServices': logoutServices})
+
+
+@csrf_protect
+def contact(request):
+    if request.method == "POST":
+        email = request.POST.get('email')
+        fullname = request.POST.get('fullname')
+        desc = request.POST.get('desc')
+        # date= request.POST.get('date')
+        contact = Contact(email=email, fullname=fullname,
+                          desc=desc, date=datetime.today())
+        contact.save()
+        messages.success(
+            request, "Your information is submitted successfully. ")
+
+    return render(request, "contact.html")
+    # return HttpResponse("This is the contactpage created from Django..! ")
+
+
+@csrf_protect
+def signup(request):
+    if request.method == 'POST':
+        form = signupform(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+        # else:
+        #     return redirect('logouthome')
+    else:
+        form = signupform()
+    return render(request, 'signup.html', {'form': form})
+
+
+@csrf_protect
+def Login(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, "Invalid credentials. Please try again.")
+            return render(request, "login.html")
+    else:
+        messages.error(request, "Please login for more details.")
+        return render(request, 'login.html')
+
+
+@csrf_protect
+def gallery(request):
+    if request.method == 'POST':
+        property_form = PropertyDetailsForm(request.POST, request.FILES)
+        property_instance = property_form.save(commit=False)  # Don't save yet
+        property_instance.owner = request.user  # Set the owner to the logged-in user
+        property_instance = property_form.save()
+        # form_type = request.POST.get('attachedbathroom')
+        # form_type=
+        # print(form_type.errors)
+
+        if request.POST.get('attachedbathroom'):
+            form = RoomsDetailsForm(request.POST)
+            if form.is_valid():
+                room = form.save(commit=False)
+                room.property = property_instance
+                room.save()
+                messages.success(
+                    request, "Your information is submitted successfully. ")
+            else:
+                print(form.errors)
+
+        elif request.POST.get('size'):
+            form = HousesDetailsForm(request.POST)
+            if form.is_valid():
+                house = form.save(commit=False)
+                # property_id = request.POST.get('property_id')
+                # property_obj = Property.objects.get(id=property_id)
+                house.property = property_instance
+                house.save()
+                messages.success(
+                    request, "Your information is submitted successfully. ")
+            else:
+                print(form.errors)
+
+    else:
+        property_form = PropertyDetailsForm()
+        # print(form.errors)
+
+    return render(request, 'gallery.html', {'property_form': property_form})
